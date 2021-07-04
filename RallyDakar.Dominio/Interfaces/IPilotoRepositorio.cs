4db1a602c0fa2fd0d84dc447f2dcf38a0f525dc9
Rafael <rwribeiro.php@gmail.com>
2021-07-04 02:49:06 -0300

    Criado primeiro repositorio e controller

---
+++ RallyDakar.Dominio/Interfaces/IPilotoRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using RallyDakar.Dominio.Entidades;
+using System.Collections.Generic;
+
+namespace RallyDakar.Dominio.Interfaces {
+    public interface IPilotoRepositorio {
+        void Adicionar(Piloto piloto);
+
+        IEnumerable<Piloto> ObterTodos();
+
+        IEnumerable<Piloto> ObterTodosPorNome(string nome);
+    }
+}
---
+++ RallyDakar.Dominio/Repositorios/PilotoRepositorio.cs
@@ -0,0 +1,29 @@
+﻿using RallyDakar.Dominio.DbContexto;
+using RallyDakar.Dominio.Entidades;
+using RallyDakar.Dominio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RallyDakar.Dominio.Repositorios {
+    public class PilotoRepositorio : IPilotoRepositorio {
+        private readonly RallyDbContexto _rallyDbContexto;
+        public PilotoRepositorio(RallyDbContexto rallyDbContexto) {
+            _rallyDbContexto = rallyDbContexto;
+        }
+
+        public void Adicionar(Piloto piloto) {
+            _rallyDbContexto.Pilotos.Add(piloto);
+            _rallyDbContexto.SaveChanges();
+        }
+
+        public IEnumerable<Piloto> ObterTodos() {
+            return _rallyDbContexto.Pilotos.ToList();
+        }
+
+        public IEnumerable<Piloto> ObterTodosPorNome(string nome) {
+            return _rallyDbContexto.Pilotos.Where(p => p.Nome.Contains(nome)).ToList();
+        }
+    }
+}
